52580925fffc63c01c47e9b072fc8e975104d964
Matheus Mattos <matheusmattos.r@gmail.com>
2024-06-20 10:42:14 -0300

    Trocando o While pelo If, else if e else

--- index.js
+++ index.js
@@ -7,29 +7,22 @@ let nome = prompt("Qual é o nome do seu heroi ? ")
 let xpHeroi = 0
 xpHeroi = prompt("Qual é o seu valor de Xp Atual ? ")
 
-
-while (xpHeroi <= 1000) {
+if (xpHeroi <= 1000) {
     console.log(frase + nome + frase2 + "Ferro")
-    break
-} while (xpHeroi <= 2000 && xpHeroi >=1001) {
+} else if (xpHeroi <= 2000 && xpHeroi >=1001) {
     console.log(frase + nome + frase2 + "Bronze")
-    break
-} while (xpHeroi <= 5000 && xpHeroi >=2001) {
+} else if (xpHeroi <= 5000 && xpHeroi >=2001) {
     console.log(frase + nome + frase2 + "Prata")
-    break
-} while (xpHeroi <= 7000 && xpHeroi >=5001) {
+} else if (xpHeroi <= 7000 && xpHeroi >=5001) {
     console.log(frase + nome + frase2 + "Ouro")
-    break
-} while (xpHeroi <= 8000 && xpHeroi >=7001) {
+} else if (xpHeroi <= 8000 && xpHeroi >=7001) {
     console.log(frase + nome + frase2 + "Platina")
-    break
-} while (xpHeroi <= 9000 && xpHeroi >=8001) {
+} else if (xpHeroi <= 9000 && xpHeroi >=8001) {
     console.log(frase + nome + frase2 + "Ascendente")
-    break
-} while (xpHeroi <= 10000 && xpHeroi >=9001) {
+} else if (xpHeroi <= 10000 && xpHeroi >=9001) {
     console.log(frase + nome + frase2 + "Imortal")
-    break
-} while (xpHeroi > 10000) {
+} else if (xpHeroi > 10000) {
     console.log(frase + nome + frase2 + "Radiante")
-    break
+} else {
+    console.log("Coloque com valor de xp valido para o seu Heroi")
 }
